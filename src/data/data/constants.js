554224c4d1b5a85d7e5da25d4ac0981d8bdec2a1
chandrakant tripathi <chandrakanttripathi192005@gmail.com>
2025-04-24 20:53:29 +0530

    new update

--- src/data/data/constants.js
+++ src/data/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/192005chandrakant",
   resume:
-    "https://drive.google.com/file/d/1-i7TONlZwOi3I6AuYAt-jxx7lAfn2p1t/view?usp=sharing",
+    "https://drive.google.com/file/d/1XSu1vs5-L3OyX4uGGH-Sy-JtkAZ5MGox/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/chandrakant-tripathi192005/",
   twitter: "https://x.com/Chandrakant019",
   insta: "https://www.instagram.com/c15_adarash/?hl=en",
